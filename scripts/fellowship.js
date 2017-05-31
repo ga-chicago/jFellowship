a165b989b63e2a33e33f5a1be7c1be9820fc693f
Payne Richards <paynerichards@Paynes-MacBook-Pro.local>
2017-05-31 16:47:27 -0500

    initial commit completed 1-6

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -17,41 +17,69 @@ var buddies = [
 ];
 
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
-var body = document.body;
+var body = $('body');
 var section = '<section></section>';
 
 function makeMiddleEarth() {
   // your answers here
+  body.append('<section id="middle-earth"></section>')
+  $.each(lands, function(i, landName){
+    $('#middle-earth').append('<article id="' + landName + '""><h1>' + landName + '</h1></article>')
+  })
 }
 
 makeMiddleEarth();
 
+var rivendell = $('#Rivendell');
+var theShire = $($('article')[0]);
+var mordor = $('#Mordor');
+
 function makeHobbits(){
   // your answers here
+  theShire.append('<ul id="hobbits"></ul>')
+  $.each(hobbits, function(i, hobbitName){
+    $('#hobbits').append('<li class="hobbit">' + hobbitName + '</li>')
+  })
+
 }
 
 makeHobbits();
 
+var frodo = $($('.hobbit')[0]);
+var sam = $($('.hobbit')[1]);
+var theRing = $('<div id="the-ring" class="magic-imbued-jewelry"></div>')
+
 function keepItSecretKeepItSafe(){
   // your answers here
+  frodo.append(theRing)
 }
 
 keepItSecretKeepItSafe();
 
 function makeBuddies(){
   // your answers here
+  rivendell.append('<aside><ul id="buddies"></ul></aside>')
+  $.each(buddies, function(i, budName){
+    $('#buddies').append('<li class="buddy">' + budName + '</li>')
+  })
 }
 
 makeBuddies();
 
+var gandalf = $($('.buddy')[0])
+var boromir = $($('.buddy')[4])
+var aragorn = $($('.buddy')[3])
+
 function beautifulStranger(){
   // your answers here
+  aragorn.text('Aragorn')
 }
 
 beautifulStranger();
 
 function leaveTheShire(){
   // your answers here
+  rivendell.append($("#hobbits"))
 }
 
 leaveTheShire();
